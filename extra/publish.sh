057ee6f9e5125c53b252f9b46b304b525cc92f9d
Magus <magus@bgforge.net>
2020-05-06 21:37:31 +0700

    added travis packaging

---
+++ extra/publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -xeu -o pipefail
+
+# release?
+if [ -n "$TRAVIS_TAG" ]; then # tag found: releasing
+  export version="$TRAVIS_TAG"
+else
+  export version="git$TRAVIS_COMMIT"
+fi
+
+zip -r "${mod_name}_${version}.zip" *
