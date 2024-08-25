bfb88752685e898979ef80a5a6d555f14ee8dd0c
Magus <magus@bgforge.net>
2024-08-25 17:15:11 +0700

    make shellcheck happy

--- extra/package.sh
+++ extra/package.sh
@@ -4,20 +4,21 @@ set -xeu -o pipefail
 
 short_sha="$(git rev-parse --short HEAD)"
 version="git$short_sha"
-if [[ ! -z "${GITHUB_REF-}" ]]; then
-  if echo "$GITHUB_REF" | grep "refs/tags"; then # tagged
-    version="$(echo $GITHUB_REF | sed 's|refs\/tags\/||')"
-  fi
+if [[ -n "${GITHUB_REF-}" ]]; then
+    if echo "$GITHUB_REF" | grep "refs/tags"; then # tagged
+        # shellcheck disable=SC2001  # sed is more readable
+        version="$(echo "$GITHUB_REF" | sed 's|refs\/tags\/||')"
+    fi
 fi
 
 for q in hq lossless_wav lossless_wavpack lossless_flac; do
-  fname="fallout_music_${q}_${version}.zip"
-  cd "$q"
-  rm -f readme.md
-  for f in 03wrldmp.*; do 
-    cp -- "$f" "03world${f#03wrldmp}"
-  done
-  zip "$fname" *
-  mv "$fname" ..
-  cd ..
+    fname="fallout_music_${q}_${version}.zip"
+    cd "$q"
+    rm -f readme.md
+    for f in 03wrldmp.*; do
+        cp -- "$f" "03world${f#03wrldmp}"
+    done
+    zip "$fname" ./*
+    mv "$fname" ..
+    cd ..
 done
